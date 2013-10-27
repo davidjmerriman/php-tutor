0da9ca6c0c23367c5048b73cd89af5951d6aeeb5
David Merriman <dmerriman@propertysolutions.com>
2013-10-27 12:10:17 -0600

    Add TODOs to lessons.php controller

--- application/controllers/lessons.php
+++ application/controllers/lessons.php
@@ -3,20 +3,31 @@
 class Lessons extends CI_Controller {
 
 	public function index() {
+		// TODO: The code that loads the lessons should be moved to the 
+		//       model layer, so we can easily switch to a database at
+		//       some point without changing controller code
 		$data['lessons'] = json_decode(file_get_contents('./lessons/lessons.json'), true);
 
 		$this->load->view('view_lessons.php', $data);
 	}
 
 	public function view($lesson) {
+		// TODO: load lesson data through model layer
 		$this->load->view('view_lesson.php');
 	}
 
 	public function test($lesson) {
+		// TODO: load lesson data, evaluate against submitted input, return output
 		echo "Test $lesson";
 	}
 
 	public function grade($lesson) {
+		// TODO: load lesson data, evaluate against all datasets, return score and
+		//       details on which datasets passed/failed, execution time per dataset,
+		//       and other information
 		echo "Grade $lesson";
+
+		// TODO: once we have user profiles, we can log their grades for use 
+		//       throughout the system
 	}
 }
